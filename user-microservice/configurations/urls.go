0291ac9b6fb1ef964043f8ae409c8fa779f99830
Andrey Vorobev <andreypremiere@mail.ru>
2026-04-20 09:11:14 +0700

    old unnecessary files were deleted

--- user-microservice/configurations/urls.go
+++
@@ -1,8 +0,0 @@
-package configurations
-
-// Пути для обращения к другим микросервисам
-
-// Room-microservice
-const BaseRoomUrl string = "http://room-microservice:81"
-
-const GetRoomIdByUserId string = BaseRoomUrl + "/getRoomIdByUserId"
--- user-microservice/contracts/responses/user-contract-response.go
+++
@@ -1,9 +0,0 @@
-package responses
-
-import "github.com/google/uuid"
-
-type UserVerifyResponse struct {
-	UserId uuid.UUID `json:"userId"`
-	AccessToken string `json:"accessToken"`
-	RefreshToken string `json:"refreshToken"`
-}
