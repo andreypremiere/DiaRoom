be5ceb5eedd6a71ef475dcc510ef814b794756cb
Andrey Vorobev <andreypremiere@mail.ru>
2026-05-14 10:25:16 +0700

    video note was done

--- diary-microservice/services/diary-service.go
+++ diary-microservice/services/diary-service.go
@@ -213,6 +213,79 @@ func (s *DiaryService) CreateMessage(ctx context.Context, roomId uuid.UUID, req 
 		}
 
 		return messageResponse, nil
+	} else if req.MsgType == "video_note" {
+		if len(req.Attachments) != 1 {
+			return nil, apperrors.ErrInternal
+		}
+
+		// Создаем новый объект сообщения
+		messageUUID := uuid.New()
+		status := "sending"
+
+		newMessage := &models.Message{
+			ID: messageUUID,
+			RoomID: roomId,
+			MsgType: req.MsgType,
+			Content: req.Content,
+			Status: &status,
+			AttachedObjectWorkshopID: req.WorkshopFolderId,
+			AttachedObjectPostID: req.PublicationPostId,
+		}
+
+		// Формируем список ответов
+		attachmentsCreating := make([]*responses.AttachmentUploadItem, 0, len(req.Attachments))
+
+		// Создаем список вложений
+		attachments := make([]*models.Attachment, 0, len(req.Attachments))
+
+		for _, item := range req.Attachments {
+			// Получаем публичный и статичный ключ для файла
+			key := fmt.Sprintf("%s/%s/%s%s", roomId, messageUUID, uuid.New(), s.s3.getExtensionFromMimeType(item.MimeType))
+			_, presignedUrl, err := s.s3.GenerateUploadUrls(ctx, key, item.MimeType)
+			if err != nil {
+				return nil, apperrors.ErrInternal
+			}
+			// Получаем публичный и статичный ключ для превью
+			keyPreview := fmt.Sprintf("%s/%s/%s%s", roomId, messageUUID, uuid.New(), ".jpeg")
+			_, presignedUrlPreview, err := s.s3.GenerateUploadUrls(ctx, keyPreview, "image/jpeg")
+			if err != nil {
+				return nil, apperrors.ErrInternal
+			}
+			attachId := uuid.New()
+			newAttach := &models.Attachment{
+				ID: attachId,
+				RoomID: roomId,
+				MessageID: messageUUID,
+				AttType: item.AttType,
+				S3Key: key,
+				PreviewS3Key: &keyPreview,
+				FileSizeBytes: item.FileSizeBytes,
+				Duration: item.Duration,
+			}
+
+			attachments = append(attachments, newAttach)
+
+			attachResponse := &responses.AttachmentUploadItem{
+				AttachmentID: attachId,
+				PresignedURL: presignedUrl,
+				PresignedPreviewURL: &presignedUrlPreview,
+			}
+
+			attachmentsCreating = append(attachmentsCreating, attachResponse)
+		}
+
+		err := s.repo.CreateMessageWithAttachments(ctx, newMessage, attachments)
+		if err != nil {
+			return nil, err
+		}
+
+		messageResponse := &responses.MessageCreateResponse{
+			MessageID: messageUUID,
+			Status: status,
+			UploadItems: attachmentsCreating,
+		}
+
+		return messageResponse, nil
 	} else {
 		return nil, apperrors.ErrInvalidInput
 	}
